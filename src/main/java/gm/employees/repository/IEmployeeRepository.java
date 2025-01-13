64da6c48e2715ed34ed7fe924f0e67ae71d30836
Klee Collage <klee.collage@gmail.com>
2025-01-13 16:38:38 -0600

    feat(JPArepository): Add IEmployeeRepository, extends from JpaRepository

---
+++ src/main/java/gm/employees/repository/IEmployeeRepository.java
@@ -0,0 +1,6 @@
+package gm.employees.repository;
+
+import gm.employees.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IEmployeeRepository extends JpaRepository<Employee, Integer> {}
